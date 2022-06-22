fcd6f5ab3c2c51a0769e9ab6d1587f35712c99c0
nima <nima.khodabandelou@gmail.com>
2022-06-22 16:28:47 +0430

    Spring Initializr added

---
+++ src/main/java/com/autotradingwiz/chucknorrisjokesappv2/ChuckNorrisJokesAppV2Application.java
@@ -0,0 +1,13 @@
+package com.autotradingwiz.chucknorrisjokesappv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChuckNorrisJokesAppV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChuckNorrisJokesAppV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/autotradingwiz/chucknorrisjokesappv2/ChuckNorrisJokesAppV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autotradingwiz.chucknorrisjokesappv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChuckNorrisJokesAppV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
